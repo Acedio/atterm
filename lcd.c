d5c5fa6b9a014d8926e6b06fe7ff3b4f65975f6c
Josh Simmons <josh.simmons@gmail.com>
2020-04-01 23:32:14 -0700

    Blinking cursor! But doesn't look like two lines?

--- lcd.c
+++ lcd.c
@@ -4,11 +4,9 @@
 
 #define LCD_E 0x80
 
-#define LCD_READ 0x40
-#define LCD_WRITE 0x00
+#define LCD_RW_READ 0x40
 
-#define LCD_DR 0x20
-#define LCD_IR 0x00
+#define LCD_RS_DR 0x20
 
 #define LCD_LED 0x10
 
@@ -32,17 +30,45 @@ void lcd_out(unsigned char address, unsigned char flags) {
 }
 
 void lcd_wait() {
-  while (PINB & _BV(PINB4)) {
-  }
+  // Can shorten these to 8 bits (no address).
+  do {
+    lcd_out(0, LCD_RW_READ);
+    lcd_out(0, LCD_E | LCD_RW_READ);
+  } while (PINB & _BV(PINB4));
+}
+
+void lcd_write(unsigned char address, unsigned char flags) {
+  // Writes look to be triggered on E falling.
+  lcd_out(address, flags | LCD_E);
+  lcd_out(address, flags & ~LCD_E);
+}
+
+void lcd_init() {
+  lcd_wait();
+
+  // 4 bit mode is default, so need to send two bits.
+  // 8 bits, 2 lines, 5x8
+  lcd_write(0x03, 0);
+  lcd_write(0x08, 0);
+  lcd_wait();
+
+  lcd_write(0x0F, 0);
+  lcd_wait();
 }
 
 int main(void) {
   DDRB = _BV(DDB1) | _BV(DDB2) | _BV(DDB3) | ~_BV(DDB4);
 
-  lcd_out(0, LCD_IR | LCD_READ | LCD_LED);
-  lcd_out(0, LCD_E | LCD_IR | LCD_READ | LCD_LED);
+  lcd_init();
 
+  /*
+  lcd_out(0x0E, LCD_E | LCD_IR);
+  lcd_out(0x0E, LCD_IR);
+
+  lcd_out(0, LCD_E | LCD_IR | LCD_RW_READ);
+  lcd_out(0, LCD_IR | LCD_RW_READ);
   lcd_wait();
+  */
 
   while (1) {
     lcd_out(0, LCD_LED);
